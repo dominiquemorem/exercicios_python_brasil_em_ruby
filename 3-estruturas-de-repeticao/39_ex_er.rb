f347bc0da14cfe74a5157313572c720bc13e4fce
Dominique Morem <domi_castro@hotmail.com>
2025-07-07 16:33:31 -0300

    Inclusão da resolução das questões 42 e 50

--- 3-estruturas-de-repeticao/39_ex_er.rb
+++ 3-estruturas-de-repeticao/39_ex_er.rb
@@ -4,3 +4,4 @@
 # representando a sua altura em centímetros. Encontre o aluno
 # mais alto e o mais baixo. Mostre o número do aluno mais
 # alto e o número do aluno mais baixo, junto com suas alturas.
+
--- 3-estruturas-de-repeticao/50_ex_er.rb
+++ 3-estruturas-de-repeticao/50_ex_er.rb
@@ -1,3 +1,22 @@
 # EXERCÍCIO 50 - ESTRUTURAS DE REPETICAO
 # Sendo H = 1 + 1/2 + 1/3 + 1/4 + ... + 1/N.
 # Faça um programa que calcule o valor de H com N termos.
+
+array_soma = []
+array_interface = []
+
+puts 'Digite o numero de Ns: '
+num = gets.chomp.to_i
+
+if num > 0 
+
+(1..num).each do |i|
+  array_soma.push(1.to_f/i)
+  array_interface.push("1/#{i}")
+end
+
+puts " H = #{array_interface.inspect} = #{array_soma.sum}"
+
+else 
+  puts 'Input inválido...'
+end
